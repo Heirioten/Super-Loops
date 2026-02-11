e2d77037e8028bdd860278dfe7798d58433ee205
Amanda Keith <akeith@clarku.edu>
2026-02-11 11:53:49 -0500

    miniscual changes

--- Assets/Scripts/Project/Level_Editor/GS_Data.cs
+++ Assets/Scripts/Project/Level_Editor/GS_Data.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d7aa5a1fbb2f12e2c6abd84a525f38b167319e4fa35b24b520df239ff4c125c
-size 2031
+oid sha256:e5452f9156d09b36ef0a361ca78a132c2bb57f41a95b4067aecc52cca4dbd2d9
+size 1965
---
+++ Assets/Scripts/Project/Level_Editor/Start.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a8a96836d019603d4ae783a48a551720b8b38796948c8f7c8bd228d4b1f75d6
+size 54
