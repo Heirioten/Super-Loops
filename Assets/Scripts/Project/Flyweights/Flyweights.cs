8bc408b52cc67dce2f77e46ed4be99f73c64abc4
Nicholas <nicholasjohnlyons@gmail.com>
2026-03-22 03:48:52 -0400

    Cart art integration with game loop

--- Assets/Scripts/Project/Flyweights/Flyweights.cs
+++ Assets/Scripts/Project/Flyweights/Flyweights.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2382e4dbbaf0f8d282062f2f0ed1b6dc36d3880a174917fafb27d55266498f28
-size 1027
+oid sha256:8b2688e85f454bb3440bf63957b3e72af6ddacd75951cc4607b84ed2708e89e8
+size 1028
--- Assets/Scripts/Project/Simulator/Cart.cs
+++ Assets/Scripts/Project/Simulator/Cart.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be53552e9314b9c539119ada3dbd25929ba6ee979f797c4271e16457a5774c81
-size 2486
+oid sha256:a87b66c8bbe301a5baf9a1f1ff08397ab575488ef71c4853f4d0c5f391e5ff3f
+size 2663
